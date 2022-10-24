fc6cc1035d6525207bd4b493a308fa69a1c64dcd
llopFilms <smdalbars@gmail.com>
2022-10-24 17:51:53 +0200

    Inicialització projecte amb Creat React App

---
+++ sprint6/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import NormalizeModern from "./components/NormalizeModern";
+
+function App() {
+  return (
+    <div className="App">
+      <NormalizeModern />
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
