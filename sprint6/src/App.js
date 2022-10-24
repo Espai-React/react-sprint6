dc11841bf99da488b8d5674647cbb79b5d2a91ad
llopFilms <smdalbars@gmail.com>
2022-10-24 20:10:20 +0200

    Exercici 2

--- sprint6/src/App.js
+++ sprint6/src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import NormalizeModern from "./components/NormalizeModern";
 import { Escena } from "./components/escena/Escena";
+import { textosHistoria } from "./data/textosHistoria";
 
 function App() {
   return (
     <div className="App">
       <NormalizeModern />
       <div>
-        <Escena />
+        {textosHistoria.map((element) => (
+          <Escena key={element.id} text={element.text} />
+        ))}
       </div>
     </div>
   );
